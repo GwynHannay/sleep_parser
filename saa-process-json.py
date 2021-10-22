66acba12369ce7581863b2db225fd6ac66886597
Gwyn <gwyn@hannay.id.au>
2021-10-23 06:32:44 +0800

    design next steps

--- saa-process-json.py
+++ saa-process-json.py
@@ -1,20 +1,34 @@
 import json
 
 def read_it(json_path):
+    json_array = []
+
     with open(json_path) as f:
         contents = json.load(f)
         
-    i = 0
+    entries = {}
+    d = {}
     for record in contents:
-        if i == 0:
-            for key in record:
-                print("lerlll {}".format(key))
-                print("brrr {}".format(record[key]))
-        else:
-            break
+        j = 0
+        for key in record:
+            if j == 0:
+                id = record[key]
+            else:
+                entries[j-1] = {'key': key, 'val': record[key]}
 
-        i = 1
+            j = j + 1
+
+        d = {'id': id, 'entries': entries}
+        json_array.append(d)
+
+    result = [json.dumps(record) for record in json_array]
+
+    with open(output_path, 'w', encoding='utf-8') as jsonf:
+        for item in result:
+            jsonf.write(''.join(item))
+            jsonf.write('\n')
 
 json_path = r'sleep-as-android/json/2021-08-10_sleep-export.json'
+output_path = r'sleep-as-android/json/sleep-key-val.json'
 
 read_it(json_path)
